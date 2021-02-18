324eb5d5e06848fd2374a63159a1395f368e1b45
Vlad Kysarets <vladd.kys@gmail.com>
2021-02-18 14:13:56 +0200

    fix

--- src/const/api.js
+++ src/const/api.js
@@ -1,7 +1,9 @@
 export const DOMAIN = 'https://api.deezer.com';
 
+const prefix = process.env.NODE_ENV === 'production' ? '/api' : '';
+
 // Artist
-export const SEARCH_ARTIST = '/api/search/artist'; // GET
-export const GET_ARTIST = '/api/artist/'; // GET  (artist/{id})
-export const GET_ARTIST_TOP_LIST = ['/api/artist/', '/top']; // GET (artist/{id}/top)
-export const GET_ARTIST_ALBUMS = ['/api/artist/', '/albums']; // GET (artist/{id}/albums)
+export const SEARCH_ARTIST = prefix + '/search/artist'; // GET
+export const GET_ARTIST = prefix + '/artist/'; // GET  (artist/{id})
+export const GET_ARTIST_TOP_LIST = [prefix + '/artist/', '/top']; // GET (artist/{id}/top)
+export const GET_ARTIST_ALBUMS = [prefix + '/artist/', '/albums']; // GET (artist/{id}/albums)
