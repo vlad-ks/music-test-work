0ce5ef334b9dadcf1c2dd82b8820676f5673d207
Vlad Kysarets <vladd.kys@gmail.com>
2021-02-18 13:38:23 +0200

    test

--- src/const/api.js
+++ src/const/api.js
@@ -1,7 +1,7 @@
 export const DOMAIN = 'https://api.deezer.com';
 
 // Artist
-export const SEARCH_ARTIST = '/search/artist'; // GET
-export const GET_ARTIST = '/artist/'; // GET  (artist/{id})
-export const GET_ARTIST_TOP_LIST = ['/artist/', '/top']; // GET (artist/{id}/top)
-export const GET_ARTIST_ALBUMS = ['/artist/', '/albums']; // GET (artist/{id}/albums)
+export const SEARCH_ARTIST = 'https://api.deezer.com/search/artist'; // GET
+export const GET_ARTIST = 'https://api.deezer.com/artist/'; // GET  (artist/{id})
+export const GET_ARTIST_TOP_LIST = ['https://api.deezer.com/artist/', '/top']; // GET (artist/{id}/top)
+export const GET_ARTIST_ALBUMS = ['https://api.deezer.com/artist/', '/albums']; // GET (artist/{id}/albums)
